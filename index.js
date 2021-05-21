abcc20e0f7f8793d2de3232b36ef0644ddf4b978
Shiva Chaubey <78716275+Shiv-flexhub@users.noreply.github.com>
2021-05-21 21:01:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+setInterval(()=> {
+    d= new Date();
+    hrs=d.getHours();
+    min=d.getMinutes();
+    sec=d.getSeconds();
+    hrotation= 30*hrs + min/2;
+    mrotation= 6*min;
+    srotation= 6*sec;
+    hour.style.transform=`rotate(${hrotation}deg)`;
+    minute.style.transform=`rotate(${mrotation}deg)`;
+    seconds.style.transform=`rotate(${srotation}deg)`;
+}, 1000);
